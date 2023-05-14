5488d7a1cb0b95ec09ac9080acd2cfa4a81ad81e
nganchaoxin <kimnganphamsu01@gmail.com>
2023-05-14 09:47:08 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <EmployeesList/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeesList from './components/EmployeeList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeesList
   }
 }
 </script>
